de48c2d9539ab533cf5f1b9c2c1aa29caf1a446d
islarobot <islarobots@gmail.com>
2020-04-11 20:34:30 +0200

    Create temperature_sensor_diferential.ino

---
+++ temperature_sensor_diferential.ino
@@ -0,0 +1,58 @@
+/*
+https://create.arduino.cc/projecthub/TheGadgetBoy/ds18b20-digital-temperature-sensor-and-arduino-9cc806
+
+https://electronics.stackexchange.com/questions/62324/arduino-controlled-pwm-pc-fan
+*/
+
+/********************************************************************/
+// First we include the libraries
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+/********************************************************************/
+// Data wire is plugged into pin 2 on the Arduino 
+#define ONE_WIRE_BUS 2 
+/********************************************************************/
+// Setup a oneWire instance to communicate with any OneWire devices  
+// (not just Maxim/Dallas temperature ICs) 
+OneWire oneWire(ONE_WIRE_BUS); 
+
+/********************************************************************/
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+/********************************************************************/ 
+float t1;
+float t2;
+float d;
+
+void setup(void) 
+{ 
+ // start serial port 
+ Serial.begin(9600); 
+ //Serial.println("Dallas Temperature IC Control Library Demo"); 
+ // Start up the library 
+ sensors.begin(); 
+} 
+void loop(void) 
+{ 
+ // call sensors.requestTemperatures() to issue a global temperature 
+ // request to all devices on the bus 
+/********************************************************************/
+ //Serial.print(" Requesting temperatures..."); 
+ sensors.requestTemperatures(); // Send the command to get temperature readings 
+ //Serial.println("DONE"); 
+/********************************************************************/
+
+t1 = sensors.getTempCByIndex(0);
+t2 = sensors.getTempCByIndex(1);
+
+d = abs(t1-t2);
+
+ Serial.println(d); 
+
+
+ 
+ // Why "byIndex"?  
+   // You can have more than one DS18B20 on the same bus.  
+   // 0 refers to the first IC on the wire 
+   delay(300); 
+} 
